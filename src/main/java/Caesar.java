69894744a9e6d5d6c9a3a5e1aa127a54f00ddb80
MetalTurtle <dekolis1218@gmail.com>
2020-09-22 22:18:36 -0700

    Finish Caeser code

--- src/main/java/Caesar.java
+++ src/main/java/Caesar.java
@@ -12,19 +12,25 @@ public class Caesar {
     public static String decryptCaesar(String message) {
         StringBuilder output = new StringBuilder();
         for (char c : message.toCharArray()) {
-            output.append((char) ((c <= 'Z' && c >= 'A') ? ((c + 10) % 26) + 65 : (c <= 'z' && c >= 'a') ? ((c + 4) % 26) + 97 : c));
+            output.append((char) ((c <= 'Z' && c >= 'A') ? ((c - 42) % 26) + 65 : (c <= 'z' && c >= 'a') ? ((c - 74) % 26) + 97 : c));
         }
         return output.toString();
     }
 
     public static String encryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        StringBuilder output = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            output.append((char) ((c <= 'Z' && c >= 'A') ? ((c - 65 + key) % 26) + 65 : (c <= 'z' && c >= 'a') ? ((c - 97 + key) % 26) + 97 : c));
+        }
+        return output.toString();
     }
 
     public static String decryptCaesarKey(String message, int key) {
-        return message;
-        // REPLACE THIS WITH YOUR CODE
+        StringBuilder output = new StringBuilder();
+        for (char c : message.toCharArray()) {
+            output.append((char) ((c <= 'Z' && c >= 'A') ? ((c - 39 - (key % 26)) % 26) + 65 : (c <= 'z' && c >= 'a') ? ((c - 71 - (key % 26)) % 26) + 97 : c));
+        }
+        return output.toString();
     }
 
 
@@ -33,23 +39,12 @@ public class Caesar {
 
         System.out.println("Would you like to \"encrypt\" or \"decrypt\" a message?");
         String command = scan.nextLine().trim().toLowerCase();
-
-        if (command.equals("encrypt")) {
-            System.out.println("Please enter your message to be encrypted: ");
-            String message = scan.nextLine();
-            System.out.println("Here is your encrypted message: ");
-            System.out.println(encryptCaesar(message));
-        }
-        else if (command.equals("decrypt")) {
-            System.out.println("Please enter your message to be decrypted: ");
-            String message = scan.nextLine();
-            System.out.println("Here is your decrypted message: ");
-            System.out.println(decryptCaesar(message));
-        }
-        else {
-            System.out.println("Unknown command; please type either \"encrypt\" or \"decrypt\"");
-        }
-
+        System.out.println("Please enter your message to be " + command + "ed: ");
+        String message = scan.nextLine().trim();
+        System.out.println("Please enter the key for your message: ");
+        int key = scan.nextInt();
+        System.out.println("Here is your " + command + "ed message: ");
+        System.out.println((command.equals("encrypt")) ? encryptCaesarKey(message, key) : (command.equals("decrypt")) ? decryptCaesarKey(message, key) : "Error");
         scan.close();
     }
 }
